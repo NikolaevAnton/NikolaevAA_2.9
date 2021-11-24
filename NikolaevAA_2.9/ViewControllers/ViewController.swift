c4f44f0cd467ff907804bd5d240b937c54e6ca3e
NikolaevAnton <mjop41@gmail.com>
2021-11-24 17:54:30 +0300

    final commit

--- NikolaevAA_2.9/ViewControllers/ViewController.swift
+++ NikolaevAA_2.9/ViewControllers/ViewController.swift
@@ -18,16 +18,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setModelValue()
+        currentModelValue()
     }
 
     @IBAction func playAction(_ sender: Any) {
-        setModelValue()
+        currentModelValue()
+        playView.animation = ModelForSpring.preset
+        playView.curve = ModelForSpring.curve
+        playView.force = ModelForSpring.force
+        playView.duration = ModelForSpring.duration
+        playView.delay = ModelForSpring.delay
+        playView.animate()
         ModelForSpring.setNewValue()
         playButton.setTitle(ModelForSpring.preset, for: .normal)
     }
     
-    private func setModelValue(){
+    private func currentModelValue(){
         presetLabel.text = "preset: \(ModelForSpring.preset)"
         curveLabel.text = "curve: \(ModelForSpring.curve)"
         forceLabel.text = "force: \(ModelForSpring.force)"
