3195268f0dbabbdf17b7badcae4fb5f1f5df7512
NikolaevAnton <mjop41@gmail.com>
2021-11-24 14:46:09 +0300

    Initial Commit

---
+++ NikolaevAA_2.9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NikolaevAA_2.9
+//
+//  Created by Anton Nikolaev on 24.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
